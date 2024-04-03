14fdeb6bd847baf095f7d0e277fb0ec47547e171
Yonathan <yonathanlie2010@gmail.com>
2024-04-03 16:46:27 +0700

    feat:change endpoint method to use controller, add vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import {createApp} from "vue/dist/vue.esm-bundler.js";
+import mycomponent from "../views/components/vueComponents/MyComponent.vue";
+const app = createApp({
+    components: {
+        'my-component' : mycomponent,
+    }
+});
+
+app.mount("#app");
